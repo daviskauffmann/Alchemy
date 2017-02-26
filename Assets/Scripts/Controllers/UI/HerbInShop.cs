2a5c6c69dc2de835e74940efaa5a4753badf2e71
daviskauffmann <daviskauffmann@gmail.com>
2017-02-26 00:36:54 -0500

    UI

---
+++ Assets/Scripts/Controllers/UI/HerbInShop.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Alchemy.Models;
+
+namespace Alchemy.Controllers
+{
+    public class HerbInShop : MonoBehaviour
+    {
+        public Herb herb;
+
+        [SerializeField]
+        Text _amount = null;
+        [SerializeField]
+        Text _name = null;
+        [SerializeField]
+        Text _rarity = null;
+		[SerializeField]
+		Text[] _effects = null;
+
+        void Start()
+        {
+            _name.text = herb.Name;
+            _rarity.text = herb.Rarity.ToString();
+            switch (herb.Rarity)
+            {
+                case Rarity.Common:
+                    _rarity.color = Color.white;
+                    break;
+                case Rarity.Uncommon:
+                    _rarity.color = Color.green;
+                    break;
+                case Rarity.Rare:
+                    _rarity.color = Color.blue;
+                    break;
+                default:
+                    _rarity.color = Color.white;
+                    break;
+            }
+			for (int i = 0; i < herb.Effects.Length; i++)
+			{
+				_effects[i].text = herb.Effects[i].Name;
+			}
+        }
+
+        void Update()
+        {
+            _amount.text = herb.Amount.ToString();
+        }
+
+        public void Discard()
+        {
+            GameManager.World.Shop.DiscardIngredient(GameManager.World.GetHerbPrototype(herb.Name));
+        }
+    }
+}
---
+++ Assets/Scripts/Models/Employees/Herbalist.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alchemy.Models
+{
+    [Serializable]
+    public class Herbalist : Employee
+    {
+        [SerializeField]
+        Region _regionToSearch;
+        [SerializeField]
+        int _excursions;
+        [SerializeField]
+        int _herbsFound;
+        [SerializeField]
+        int _rareHerbsFound;
+
+        public Region RegionToSearch
+        {
+            get { return _regionToSearch; }
+            set { _regionToSearch = value; }
+        }
+
+        public Herbalist(World world, string name, int salary)
+            : base(world, "Herbalist", name, salary)
+        {
+            _regionToSearch = Region.Plains;
+        }
+
+        public override void StartWorking()
+        {
+            base.StartWorking();
+            _world.HourChanged += FindHerb;
+        }
+
+        public override void StopWorking()
+        {
+            base.StopWorking();
+            _world.HourChanged -= FindHerb;
+        }
+
+        void FindHerb(object sender, IntEventArgs e)
+        {
+            if (_world.Random.Next(0, 100) < 10)
+            {
+                var herbs = new List<Herb>();
+                for (int i = 0; i < _world.HerbDatabase.Length; i++)
+                {
+					/*for (int j = 0; j < _world.HerbDatabase[i].Regions.Length; j++)
+                    {
+                        if (_world.HerbDatabase[i].Regions[j] == RegionToSearch)
+                        {
+                            herbs.Add(_world.HerbDatabase[i]);
+                        }
+                    }*/
+					herbs.Add(_world.HerbDatabase[i]);
+				}
+                if (herbs.Count > 0)
+                {
+                    var herb = herbs[_world.Random.Next(herbs.Count)];
+                    _world.Shop.DeliverIngredient(herb);
+                }
+            }
+        }
+    }
+}
